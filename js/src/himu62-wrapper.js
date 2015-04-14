aece539490826994562a7cf103ef45dbb2059634
himu62 <zero.256dic@gmail.com>
2015-04-15 03:11:15 +0900

    Fixed spinner adjust Fixed: Spinner cannot adjust on window resized

--- js/src/himu62-wrapper.js
+++ js/src/himu62-wrapper.js
@@ -1,25 +1,31 @@
 Polymer('himu62-wrapper', {
-	domReady: function() {
+	circleResize: function() {
+		var root = document.querySelector('himu62-app /deep/ himu62-wrapper');
+
 		// adjust the spinner to page view
-		var pageWidth = this.$.content.offsetWidth;
-		var pageHeight = this.$.content.offsetHeight;
-		var spinnerSize = parseInt(Math.max(pageWidth, pageHeight) * 0.25);
+		var pageWidth = root.$.content.offsetWidth;
+		var pageHeight = root.$.content.offsetHeight;
+		var spinnerSize = parseInt(Math.max(pageWidth, pageHeight) * 0.2);
 		var spinnerWeight = parseInt(spinnerSize * 0.05);
-		var spinner = this.shadowRoot.querySelector('paper-spinner');
-		var spinnerCircles = this.shadowRoot.querySelectorAll('paper-spinner /deep/ .circle');
+		var spinner = root.shadowRoot.querySelector('paper-spinner');
+		var spinnerCircles = root.shadowRoot.querySelectorAll('paper-spinner /deep/ .circle');
 		spinner.style.width = spinner.style.height = spinnerSize + 'px';
 		spinner.style.top = parseInt((pageHeight - spinnerSize) / 2) + 'px';
 		spinner.style.left = parseInt((pageWidth - spinnerSize) / 2) + 'px';
 		for(var i=0; i<spinnerCircles.length; ++i) {
 			spinnerCircles.item(i).style.borderWidth = spinnerWeight + 'px';
 		}
-	},pageChanged: function(oldVal, newVal) {
+	},
+	domReady: function() {
+		this.circleResize();
+		window.onresize = this.circleResize;
+	},
+	pageChanged: function(oldVal, newVal) {
 		var loader = this.$.loader;
 		loader.url = 'partials/' + newVal.toLowerCase() + '.html.part';
 		loader.go();
 	},
 	resWrapper: function(e, res) {
-		console.log(res);
 		this.$.content.innerHTML = res.response;
 	}
 });
